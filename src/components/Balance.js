43633b261cb3d6b8cc987f22fc2d0806e0d21f3e
Deekshith Ponugoti <deekshithponugoti@gmail.com>
2023-03-02 22:31:21 +0530

    First commit

---
+++ src/components/Balance.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { GlobalContext } from '../context/GlobalState'
+import { useContext } from 'react'
+
+const Balance = () => {
+  const {transactions} = useContext(GlobalContext) 
+
+  const amounts = transactions.map(transaction => transaction.amount)
+  console.log(amounts)
+  
+  const total = amounts.reduce((acc, item) => (acc += item),0).toFixed(2)
+  return (
+    <>
+      <h4>Your Balance</h4>
+      <h1 className='balance'>{total} Rs</h1>
+    </>
+  )
+}
+
+export default Balance
---
+++ src/components/Transaction.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { GlobalContext } from '../context/GlobalState'
+import { useContext } from 'react'
+
+const Transaction = ({transaction}) => {
+  const {deleteTransaction} = useContext(GlobalContext)
+  const sign = transaction.amount < 0? '-' : '+'
+  return (
+    <li className= {transaction.amount< 0 ? 'minus' : 'plus'}>
+        {transaction.text} <span>{sign}{Math.abs(transaction.amount)} Rs</span><button onClick={() => deleteTransaction(transaction.id)} className='delete-btn'>x</button>
+    </li>
+  )
+}
+
+export default Transaction
